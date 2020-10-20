46d4e6ecf56981a0a34ad3b0c0adc6d5dc84c303
tdfyoder <tdfyoder@gmail.com>
2020-10-20 16:19:29 -0400

    Update telo.sh

--- telo.sh
+++ telo.sh
@@ -7,36 +7,46 @@
 
 echo -e "Sample\tMapped\tUnMapped\tCentrometric\tPeritelomeric\tMean_Kmer\tSD_Kmer\tMax_Kmer\tMotif kmer2\tMotif kmer2" >> results.tsv
 
-paste TARG | while read var;
+paste T2T.TARG | while read var;
 
 do
 
 var2=${var%.sam.bam}
 
+var3=${var%_T2T.sam.bam}
+
 MAPPED=`/home/yoderto/miniconda3/bin/samtools view -c "${var}"`
 
-#/home/yoderto/miniconda3/bin/samtools view -b -f 4 "${var}" > unmapped."${var2}".bam
+/home/yoderto/miniconda3/bin/samtools view -b -f 4 "${var}" > unmapped."${var2}".bam
 UNMAP=`/home/yoderto/miniconda3/bin/samtools view -c unmapped."${var2}".bam`
 
 
 #/home/yoderto/miniconda3/bin/samtools view -b -h -L centro.bed "${var}" > centro."${var2}".bam
-CENTRO=`/home/yoderto/miniconda3/bin/samtools view -c centro."${var2}".bam`
+CENTRO= 'null' # `/home/yoderto/miniconda3/bin/samtools view -c centro."${var2}".bam`
 
-#/home/yoderto/miniconda3/bin/samtools view -b -h -L pertelo.38.bed "${var}" > telo."${var2}".bam
+/home/yoderto/miniconda3/bin/samtools view -b -h -L T2T.telo.bed "${var}" > telo."${var2}".bam
+
+/home/yoderto/miniconda3/bin/samtools view telo."${var2}".bam | cut -f 10 | awk '{ print length }' >> telo.bases_"$var2".txt
+
 TELO=`/home/yoderto/miniconda3/bin/samtools view -c telo."${var2}".bam`
-
 
 kmer1=`/home/yoderto/miniconda3/bin/samtools view "${var}" | grep -E 'TTAGGGTTAGGG|AATCCCAATCCC' | wc -l`
 
+/home/yoderto/miniconda3/bin/samtools view "${var}" | grep -E 'TTAGGGTTAGGG|AATCCCAATCCC' | cut -f 10 | awk '{ print length }' >> 2mer_"$var2".txt
+
 kmer2=`/home/yoderto/miniconda3/bin/samtools view "${var}" | /home/yoderto/miniconda3/bin/agrep -2 TTAGGGTTAGGGTTAGGG | wc -l`
+
+samtools view "${var}" | /home/yoderto/miniconda3/bin/agrep -2 TTAGGGTTAGGGTTAGGG | cut -f 10 | awk '{ print length }' >> 3mer_"$var2".txt
 
 kmer2c=`/home/yoderto/miniconda3/bin/samtools view "${var}" | /home/yoderto/miniconda3/bin/agrep -2 AATCCCAATCCCAATCCC | wc -l`
 
+samtools view "${var}" | /home/yoderto/miniconda3/bin/agrep -2 AATCCCAATCCCAATCCC | cut -f 10 | awk '{ print length }' >> 3mer_"$var2".txt
+
 (( kmerplus = $kmer2 + $kmer2c ))
 
-grep -n -o  'TTAGGG' /home/yoderto/telo/fastq/"${var2}".fastq | sort -n | uniq -c | cut -d : -f 1 > ${var2}.txt
+grep -n -o  'TTAGGG' /home/yoderto/telo/fastq/"${var3}".fastq | sort -n | uniq -c | cut -d : -f 1 > ${var2}.txt
 
-grep -n -o 'AATCCC' /home/yoderto/telo/fastq/"${var2}".fastq | sort -n | uniq -c | cut -d : -f 1 >> ${var2}.txt
+grep -n -o 'AATCCC' /home/yoderto/telo/fastq/"${var3}".fastq | sort -n | uniq -c | cut -d : -f 1 >> ${var2}.txt
 
 awk '(NR>1) && ($1 > 1) ' ${var2}.txt > ${var2}1.txt
 
@@ -46,7 +56,7 @@ SD=`awk '{sum+=$1; sumsq+=$1*$1} END {print sqrt(sumsq/NR - (sum/NR)^2)}' ${var2
 
 max=`awk 'BEGIN{a=   0}{if ($1>0+a) a=$1} END{print a}' ${var2}1.txt`
 
-echo -e "$var2\t$MAPPED\t$UNMAP\t$CENTRO\t$TELO\t$AVG\t$SD\t$max\t$kmer1\t$kmerplus" >> results.tsv
+echo -e "$var2\t$MAPPED\t$UNMAP\t$CENTRO\t$TELO\t$AVG\t$SD\t$max\t$kmer1\t$kmerplus" >> T2T.results.tsv
 
 
 done;
