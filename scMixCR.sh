13320fa2c5b6f2dd9edcb575dd158e17fb088da2
tdfyoder <tdfyoder@gmail.com>
2021-07-05 15:04:46 -0400

    Update scMixCR.sh

--- scMixCR.sh
+++ scMixCR.sh
@@ -2,6 +2,8 @@
 
 seqtk trimfq bamtofastq_S1_L001_R1_001.fastq.gz -b 44 > trim_bamtofastq_S1_L001_R1_001.fastq
 
+gzip trim_bamtofastq_S1_L001_R1_001.fastq
+
 mixcr align -s hs -p rna-seq -OallowPartialAlignments=true -OsaveOriginalReads=true trim_bamtofastq_S1_L001_R1_001.fastq.gz bamtofastq_S1_L001_R2_001.fastq.gz alignments.vdjca
 
 mixcr assemblePartial alignments.vdjca alignmentsRescued_1.vdjca
@@ -12,6 +14,6 @@ mixcr extend alignmentsRescued_2.vdjca alignmentsRescued_2_extended.vdjca
 
 mixcr assemble -a alignmentsRescued_2_extended.vdjca clones.clna
 
-mixcr exportClones -c TRA -o -t clones.clna clones.txt
+mixcr exportClones -c TRB -o -t clones.clna clones.txt
 
 mixcr exportReadsForClones --id 2  clones.clna reads_of_my_clones.fastq.gz
