480965cdb8ee713837aaba1d9fe9a6d2d1b63b2d
tdfyoder <tdfyoder@gmail.com>
2021-07-06 15:14:34 -0400

    Update scMixCR.sh

--- scMixCR.sh
+++ scMixCR.sh
@@ -17,3 +17,15 @@ mixcr assemble -a alignmentsRescued_2_extended.vdjca clones.clna
 mixcr exportClones -c TRB -o -t clones.clna clones.txt
 
 mixcr exportReadsForClones --id 2  clones.clna reads_of_my_clones.fastq.gz
+
+###
+
+mixcr exportReadsForClones --id 754 1115 1430 1552 1609 1612 1623 1890 2518 2547 2898 3778 4152 4257 4278 4305 4376 4459 4470" clones.clna overlap.fastq.gz
+
+
+zcat overlap_R1.fastq.gz | grep "^@" > overlap_readlist.txt
+
+
+filterbyname.sh in=bamtofastq_S1_L001_R1_001.fastq.gz out=filtered.fastq names=overlap_readlist.txt
+
+umi_tools whitelist --stdin filtered.fastq --bc-pattern=CCCCCCCCCCCCCCCCNNNNNNNNNN --set-cell-number=40 --log2stderr > whitelist.txt
